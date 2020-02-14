8f9d05fcbc8d34678e5459ce9256c44a1a93a7c9
Audaxxy <nathan.boldy@insanitycorr.com>
2020-02-13 22:43:02 -0500

    Alternate terminal usages

--- Assets/WIP/Boldy/In-Progress/Terminal.cs
+++ Assets/WIP/Boldy/In-Progress/Terminal.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Terminal : MonoBehaviour
 {
 	public GameObject gate;
+	public bool final;
+	private bool launch = false;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -14,10 +16,17 @@ public class Terminal : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-
+		if (launch)
+			gate.GetComponent<Transform>().position = gate.GetComponent<Transform>().position + Vector3.up*0.1f;
 	}
 	public void openGate(GameObject gate)
 	{
-		Destroy(gate);
+		if (!final)
+			Destroy(gate);
+		else
+		{
+			launch = true;
+			
+		}
 	}
 }
