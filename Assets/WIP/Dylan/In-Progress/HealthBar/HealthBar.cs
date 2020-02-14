52d29f0e44e8f97489cc3fb505a0688902526c42
Dylan Moore <dylan.moore@insanitycorr.com>
2020-02-14 03:56:56 -0500

    Re-add healthbar (apparently I never pushed and lost the commit?

---
+++ Assets/WIP/Dylan/In-Progress/HealthBar/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Entity observedEntity = null;
+
+    public Image fill=null;
+
+    private void Update()
+    {
+        if (fill) {
+            if (observedEntity)
+                fill.fillAmount = ((float)observedEntity.currentHealth) / ((float)observedEntity.maxHealth);
+        } else {
+            Debug.LogWarning("[HealthBar] Fill object not specified!");
+        }
+    }
+}
---
+++ Assets/WIP/Dylan/In-Progress/HealthBar/TrainingProgress.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainingProgress : MonoBehaviour
+{
+    public Barracks linkedBarracks = null;
+
+    public Image fill;
+
+    private void Update() {
+        if (fill) {
+            if (linkedBarracks) {
+                fill.fillAmount = linkedBarracks.buildProcess.value;
+            }
+        } else {
+            Debug.LogWarning("[TrainingProgress] Fill object not specified!");
+        }
+    }
+}
